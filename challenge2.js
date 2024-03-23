78b860cd59b383d8c1c618950f2b04f62abbef1d
John <jgicheru325@gmail.com>
2024-03-23 22:54:49 +0300

    complete challenge2

--- challenge2.js
+++ challenge2.js
@@ -1,9 +1,10 @@
+// The code first prompts the user to input the speed of the car
 const prompt = require('prompt-sync')();
 const input = function () {
     console.log("Enter speed of the car.");
     return parseFloat(prompt());
 }
-
+// This is a function that calculates the demerit points.
 const speed = input();
 function demeritPoints(speed) {
     if (speed <= 70) {
